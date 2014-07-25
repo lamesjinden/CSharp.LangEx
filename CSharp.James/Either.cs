921448b1a82e9847e30cfec31b6e220f51d2f38c
james <jamesmlinden@gmail.com>
2014-07-25 01:38:46 -0400

    directoy rename

---
+++ CSharp.James/Either.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharp.James
+{
+
+    /// <summary>
+    /// Represents a value or error (as instance of Exception)
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public sealed class Either<T>
+    {
+
+        public Either(T value)
+        {
+            HasValue = true;
+            Value = value;
+        }
+
+        public Either(Exception error)
+        {
+            HasError = true;
+            Error = error;
+        }
+
+        public bool HasValue
+        {
+            get;
+            private set;
+        }
+
+        public T Value
+        {
+            get;
+            private set;
+        }
+
+        public bool HasError
+        {
+            get;
+            private set;
+        }
+
+        public Exception Error
+        {
+            get;
+            private set;
+        }
+
+        public static implicit operator Either<T>(T value)
+        {           
+            return new Either<T>(value);
+        }
+
+        public static implicit operator Either<T>(Exception exception)
+        {
+            return new Either<T>(exception);
+        }
+
+    }
+
+}
---
+++ CSharp.James/ExceptionEx.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CSharp.James
+{
+
+    public static class ExceptionEx
+    {
+
+        public static T ThrowIfNull<T>(this T t) where T : class
+        {
+            if (t == null) throw new ArgumentNullException();
+            return t;
+        }
+
+        public static T ThrowIfNull<T>(this T t, string paramName) where T : class
+        {
+            if (t == null) throw new ArgumentNullException(paramName);
+            return t;
+        }
+
+        public static T ThrowIfNull<T>(this T t, string paramName = null, string message = null) where T : class
+        {
+            if (t == null) throw new ArgumentNullException(paramName, message);
+            return t;
+        }
+
+    }
+
+}
